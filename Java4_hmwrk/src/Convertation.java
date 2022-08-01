766fa8432c4fe82af9f002256419bd4d4f1a0918
Darya Volzhankina <dariavolzhankina1431@gmail.com>
2022-08-01 23:39:04 +0300

    Начало проекта

--- Java4_hmwrk/src/Convertation.java
+++ Java4_hmwrk/src/Convertation.java
@@ -2,6 +2,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class Convertation {
+
     public static Timestamp convertLocalDateTimeToTimestamp(LocalDateTime localDateTime) {
         Timestamp timestamp = Timestamp.valueOf(localDateTime);
         return timestamp;
---
+++ Java4_hmwrk/src/interfaces/Info.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+@FunctionalInterface
+public interface Info {
+    void getInfoCharacter();
+
+    default void getInfoGame() {
+        System.out.println("It's MMORPG");
+    }
+}
