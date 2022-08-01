9a82c9d990d3c5e17d083ba3bf6d1507acf662bb
Darya Volzhankina <dariavolzhankina1431@gmail.com>
2022-08-01 23:05:48 +0300

    Начало проекта

--- Java4_homework/src/Convertation.java
+++
@@ -1,9 +0,0 @@
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
-public class Convertation {
-    public static Timestamp convertLocalDateTimeToTimestamp(LocalDateTime localDateTime) {
-        Timestamp timestamp = Timestamp.valueOf(localDateTime);
-        return timestamp;
-    }
-}
--- Java4_homework/src/interfaces/Attack.java
+++
@@ -1,6 +0,0 @@
-package interfaces;
-
-@FunctionalInterface
-public interface Attack {
-    void attack();
-}
