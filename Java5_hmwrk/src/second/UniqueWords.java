8720a4dd54982f412f17d615ac99ef18696414be
Darya Volzhankina <dariavolzhankina1431@gmail.com>
2022-08-10 00:09:13 +0300

    второе

---
+++ Java5_hmwrk/src/second/UniqueWords.java
@@ -0,0 +1,20 @@
+package second;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UniqueWords {
+    public static void main(String[] args) {
+        String str = "one two three one four three one two five six one";
+        String[] words = str.toLowerCase().split("[^\\p{L}\\p{N}]+");
+        List<String> withDuplicates = Arrays.stream(words).collect(Collectors.toList());
+        List<String> withoutDuplicates = Arrays.stream(words).distinct().collect(Collectors.toList());
+
+        for (Object o : withoutDuplicates) {
+            withDuplicates.remove(o);
+        }
+
+        System.out.println(Arrays.stream(withDuplicates.toArray()).distinct().toList());
+    }
+}
