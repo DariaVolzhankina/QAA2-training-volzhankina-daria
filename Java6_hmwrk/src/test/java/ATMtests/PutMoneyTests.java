af89e5362c8fe8c7977a809f9870d3ec17725233
Darya Volzhankina <dariavolzhankina1431@gmail.com>
2022-09-04 19:05:42 +0300

    putMoneyTests ATM

--- Java6_hmwrk/src/test/java/ATMtests/PutMoneyTests.java
+++ Java6_hmwrk/src/test/java/ATMtests/PutMoneyTests.java
@@ -1,72 +1,54 @@
-//package ATMtests;
-//
-//import exceptions.MoneyAmountException;
-//import exceptions.WrongCurrencyException;
-//import objects.ATM;
-//import objects.Cash;
-//import objects.DebitCard;
-//import org.testng.Assert;
-//import org.testng.annotations.DataProvider;
-//import org.testng.annotations.Test;
-//
-//public class PutMoneyTests {
-//    @DataProvider
-//    public Object[][] putMoneyPositiveData() {
-//        return new Object[][]{
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "1234", "rub", 10000), new Cash(1,"rub"),10001},
-//                {new ATM("Sber", "rub", 100),new DebitCard("Sber", "1111222233334444", "4563", "rub", 10000),  new Cash(10000,"rub"),20000}
-//        };
-//    }
-//
-//    @DataProvider
-//    public Object[][] putMoneyNegativeData() {
-//        return new Object[][]{
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "1234", "rub", 10000), new Cash(0,"rub")},
-//                {new ATM("Sber", "rub", 100),new DebitCard("Sber", "1111222233334444", "4563", "rub", 10000),  new Cash(-10000,"rub")}
-//        };
-//    }
-//
-//    @DataProvider
-//    public Object[][] atmLimitAfterDepositingMoneyPositiveData() {
-//        return new Object[][]{
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "1234", "rub", 10000), new Cash(1,"rub"),100001},
-//                {new ATM("Sber", "rub", 100),new DebitCard("Sber", "1111222233334444", "4563", "rub", 10000),  new Cash(10000,"rub"),10100}
-//        };
-//    }
-//
-//    @DataProvider
-//    public Object[][] putMoneyCheckCurrencyNegativeData() {
-//        return new Object[][]{
-//                {new ATM("Sber", "r", 100000),new DebitCard("Sber", "1111222233334444", "1234", "rub", 10000), new Cash(1,"rub")},
-//                {new ATM("Sber", "", 100000),new DebitCard("Sber", "1111222233334444", "4563", "", 200000), new Cash(1,"rub")},
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub ", 200000), new Cash(1,"rub")},
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "4563", null, 200000), new Cash(1,"rub")},
-//                {new ATM("Sber", " ", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub", 200000), new Cash(1," ")},
-//                {new ATM("Sber", "", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub", 200000), new Cash(1,"")},
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub", 200000), new Cash(1,null)},
-//                {new ATM("Sber", "rub", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub", 200000), new Cash(1,"rub ")},
-//                {new ATM("Sber", "rb", 100000),new DebitCard("Sber", "1111222233334444", "4563", "rub", 200000), new Cash(1,"rub")},
-//        };
-//    }
-//
-//    @Test(dataProvider = "putMoneyPositiveData")
-//    public void testWithdrawMoneyDebitPositive(ATM atm, DebitCard card, Cash cash, int expected) {
-//        Assert.assertEquals(atm.putMoney(card,cash), expected);
-//    }
-//
-//    @Test(dataProvider = "putMoneyCheckCurrencyNegativeData", expectedExceptions = WrongCurrencyException.class)
-//    public void testWithdrawMoneyDebitWrongCurrencyException(ATM atm, DebitCard card, Cash cash) {
-//        atm.putMoney(card,cash);
-//    }
-//
-//    @Test(dataProvider = "putMoneyNegativeData", expectedExceptions = MoneyAmountException.class)
-//    public void testWithdrawMoneyDebitNegative(ATM atm, DebitCard card, Cash cash) {
-//     atm.putMoney(card,cash);
-//    }
-//
-//    @Test(dataProvider = "atmLimitAfterDepositingMoneyPositiveData")
-//    public void testAtmLimitAfterDepositingMoneyPositive(ATM atm, DebitCard card, Cash cash, int atmLimitExpected ) {
-//        atm.putMoney(card,cash);
-//        Assert.assertEquals(atm.getLimit(),atmLimitExpected);
-//    }
-//}
+package ATMtests;
+
+import exceptions.MoneyAmountException;
+import exceptions.WrongCurrencyException;
+import objects.ATM;
+import objects.Cash;
+import objects.DebitCard;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static objects.enums.Banks.SBER;
+import static objects.enums.Currencies.*;
+
+public class PutMoneyTests {
+    @DataProvider
+    public Object[][] putMoneyPositiveData() {
+        return new Object[][]{
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(1,RUB),10001},
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(10000,RUB),20000}
+        };
+    }
+
+    @DataProvider
+    public Object[][] putMoneyMoneyAmountException() {
+        return new Object[][]{
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(0,RUB)},
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(-10000,RUB)}
+        };
+    }
+
+    @DataProvider
+    public Object[][] putMoneyWrongCurrencyException() {
+        return new Object[][]{
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(100,USD)},
+                {new ATM(SBER, RUB, 100000),new DebitCard(SBER, "1111222233334444", "1234", RUB, 10000), new Cash(100,EUR)}
+        };
+    }
+
+    @Test(dataProvider = "putMoneyPositiveData")
+    public void testWithdrawMoneyDebitPositive(ATM atm, DebitCard card, Cash cash, int expected) {
+        Assert.assertEquals(atm.putMoney(card,cash), expected);
+    }
+
+    @Test(dataProvider = "putMoneyWrongCurrencyException", expectedExceptions = WrongCurrencyException.class)
+    public void testWithdrawMoneyDebitWrongCurrencyException(ATM atm, DebitCard card, Cash cash) {
+        atm.putMoney(card,cash);
+    }
+
+    @Test(dataProvider = "putMoneyMoneyAmountException", expectedExceptions = MoneyAmountException.class)
+    public void testWithdrawMoneyDebitNegative(ATM atm, DebitCard card, Cash cash) {
+     atm.putMoney(card,cash);
+    }
+}
